84145ccb455c232b9d69b2ae5e95986bc51f0994
Daniel <Daniel.sabrkesh@outlook.com>
2019-09-03 15:44:25 +0200

    5-6

--- src/progv2/HelloWorld.java
+++ src/progv2/HelloWorld.java
@@ -8,7 +8,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			Scanner input = new Scanner(System.in);
-			
+			System.out.println("användarnamn?");
+		String username=	input.nextLine();
 		
 	}
 }
