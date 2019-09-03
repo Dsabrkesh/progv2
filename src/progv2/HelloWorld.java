be968a063d59398b7cc6980245072976029da493
Daniel <Daniel.sabrkesh@outlook.com>
2019-09-03 15:01:07 +0200

    scanner main

---
+++ src/progv2/HelloWorld.java
@@ -0,0 +1,15 @@
+package progv2;
+
+import java.util.Scanner;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Daniel");
+			Scanner input = new Scanner(System.in);
+			
+			input.nextLine();
+	}
+}
